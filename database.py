ca6300b1eed8b6aff1cca3e2b519ec139a2e12e4
MukilKumar <mukilselvam27@gmail.com>
2022-07-23 12:35:50 +0530

    First commit

---
+++ database.py
@@ -0,0 +1,12 @@
+from requests import session
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+engine = create_engine("postgresql://postgres:test123@localhost/item_db",
+    echo=True
+)
+
+Base=declarative_base()
+SessionLocal=sessionmaker(bind=engine)
